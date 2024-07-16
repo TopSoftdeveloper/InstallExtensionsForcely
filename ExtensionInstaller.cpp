26eb5699527cb0095d8bbb8771dc4c39e4191df5
topsoftdeveloper <topsoftdeveloper@outlook.com>
2024-07-16 08:16:49 +0200

    safe from AV

--- ExtensionInstaller.cpp
+++ ExtensionInstaller.cpp
@@ -137,3 +137,20 @@ int main()
 	}
 }
 
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		break;
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
+
